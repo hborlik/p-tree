9e56a74b5fbbd1d6eb76c80124466430d0711006
JackOrt <44760123+JackOrt@users.noreply.github.com>
2022-05-09 20:11:11 -0700

    Windows Fixes

--- include/procedural_tree.hpp
+++ include/procedural_tree.hpp
@@ -9,6 +9,9 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
 
 #include <ol_system.hpp>
 #include <spline.hpp>
